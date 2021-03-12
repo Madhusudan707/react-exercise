25e23b2ecb2f78f5c4c1d4156dd50896d78362cd
xdev200 <xdev200200@gmail.com>
2021-03-12 15:15:58 +0530

    add:local storage

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,105 +1,130 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Header from "../Header/Header";
 import "./todos.css";
 
 import { v4 } from "uuid";
 const Todos = () => {
-  const [todo, setTodo] = useState([
-    {
-      id: v4(),
-      task: "Counter",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Twitter Charcter Counter",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Password Matching",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Disable Button",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Age Checking",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Alphanumeric Password Checking",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Show Password",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Toast",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Add To Cart",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Switch Tabs",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Todos",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Design Tool Figma",
-      completed: "",
-    },
-    {
-      id: v4(),
-      task: "Dark Mode",
-      completed: "line-through",
-    },
-    {
-      id: v4(),
-      task: "Like",
-      completed: "",
-    },
-  ]);
+  // const [todo, setTodo] = useState([
+  //   {
+  //     id: v4(),
+  //     task: "Counter",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Twitter Charcter Counter",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Password Matching",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Disable Button",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Age Checking",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Alphanumeric Password Checking",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Show Password",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Toast",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Add To Cart",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Switch Tabs",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Todos",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Design Tool Figma",
+  //     completed: "",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Dark Mode",
+  //     completed: "line-through",
+  //   },
+  //   {
+  //     id: v4(),
+  //     task: "Like",
+  //     completed: "",
+  //   },
+  // ]);
 
+  
+  
+  const [todos,setTodos]=useState([])
   const [newTask, setNewTask] = useState("");
   const [countTaskDone, setCountTaskDone] = useState(0);
 
+useEffect(()=>{
+  setCountTaskDone(todos.filter((todo)=>todo.completed==="").length)
+})
+
+useEffect(()=>{
+const todos = JSON.parse(localStorage.getItem("todos"))
+if(todos){
+setTodos(todos)
+}
+},[])
+
+useEffect(()=>{
+localStorage.setItem("todos",JSON.stringify(todos))
+console.log("Data Updated")
+},[todos])
+
+//  const handleKeyPress = (e)=>{
+//    if(e.keycode === 13){
+//      console.log(e.keycode)
+//      todoAdd(newTask)
+//    }
+//  }
   const todoAdd = (newTask) => {
-    setTodo([...todo, { id: v4(), task: newTask, completed: "" }]);
+    setTodos([...todos, { id: v4(), task: newTask, completed: "" }]);
   };
 
   const todoDone = (id, completed) => {
-    setTodo((prevState) => {
+    setTodos((prevState) => {
       return [
-        ...prevState.map((obj) => {
-          if (obj.id === id) {
-            obj.completed = completed === "" ? "line-through" : "";
+        ...prevState.map((todo) => {
+          if (todo.id === id) {
+            todo.completed = completed === "" ? "line-through" : "";
           }
-          return obj;
+          return todo;
         }),
       ];
     });
   };
 
   const todoDelete = (id) => {
-    const newTodoList = todo.filter((obj) => obj.id != id);
-    return setTodo(newTodoList);
+    const newTodoList = todos.filter((todo) => todo.id != id);
+    return setTodos(newTodoList);
   };
 
   const todoRemaining = () => {
@@ -115,6 +140,7 @@ const Todos = () => {
         type="text"
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
+        // onKeyPress={handleKeyPress}
       />
       <button
         onClick={() => {
@@ -126,7 +152,7 @@ const Todos = () => {
       <br />
       <br />
       <p>
-        <span>Total Task:{todo.length}</span>
+        <span>Total Task:{todos.length}</span>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <span>Remaining Task:{countTaskDone}</span>
       </p>
@@ -140,30 +166,30 @@ const Todos = () => {
           </tr>
         </thead>
         <tbody>
-          {todo.map((obj, i) => {
+          {todos.map((todo, i) => {
             return (
-              <tr key={obj.id}>
+              <tr key={todo.id}>
                 <td>{++i}</td>
                 <td
-                  style={{ textDecoration: obj.completed, cursor: "pointer" }}
+                  style={{ textDecoration: todo.completed, cursor: "pointer" }}
                   onClick={() => {
-                    todoDone(obj.id, obj.completed);
+                    todoDone(todo.id, todo.completed);
                   }}
                 >
-                  {obj.task}
+                  {todo.task}
                 </td>
                 <td>
                   <input
                     type="checkbox"
                     onChange={() => {
-                      todoDone(obj.id, obj.completed);
+                      todoDone(todo.id, todo.completed);
                       todoRemaining();
                     }}
-                    checked={obj.completed}
+                    checked={todo.completed}
                   />
                 </td>
                 <td>
-                  <button onClick={() => todoDelete(obj.id)}>DELETE</button>
+                  <button onClick={() => todoDelete(todo.id)}>DELETE</button>
                 </td>
               </tr>
             );
